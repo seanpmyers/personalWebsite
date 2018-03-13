2fcb07e00c275b38901896026b07a8d8a51e1741
Sean <spmyers@protonmail.com>
2018-03-13 19:16:58 -0400

    adding js for language change option

---
+++ js/switchLang.js
@@ -0,0 +1,11 @@
+
+
+function switchLang (zh){
+    var zh = false;
+    if(!zh) {
+        document.getElementsByClassName('myName').innerHTML = "孟尚朴";
+    }
+    else {
+        //document.getElementsByClassName('myName').innerHTML = "Sean Myers";
+    }
+}
