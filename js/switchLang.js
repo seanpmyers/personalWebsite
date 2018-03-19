b8d2110efdc401e9e39357877ce223122aecb8ca
Sean-Laptop <sean@seans-mbp.gateway.pace.com>
2018-03-19 19:46:43 -0400

    update to home page layout and text. new js to change languages - currently only navbar. add resume download. remove globe image.

--- js/switchLang.js
+++ js/switchLang.js
@@ -3,11 +3,20 @@ zh = false;
 function switchLang (){
     if(!zh) {
         document.getElementById('text').innerHTML = "孟尚朴";
-        document.getElementById('text').innerHTML = "博客";
+        document.getElementById('text2').innerHTML = "Eng/英文";
+        document.getElementById('text3').innerHTML = "博客";
+        document.getElementById('text4').innerHTML = "计划";
+        document.getElementById('text5').innerHTML = "资讯";
+        document.getElementById('text6').innerHTML = "主页";
         zh = true;
     }
     else {
         document.getElementById('text').innerHTML = "Sean Myers";
+        document.getElementById('text2').innerHTML = "中文/Zh";
+        document.getElementById('text3').innerHTML = "Blog";
+        document.getElementById('text4').innerHTML = "Projects";
+        document.getElementById('text5').innerHTML = "About";
+        document.getElementById('text6').innerHTML = "Home";
         zh = false;
     }
 }
