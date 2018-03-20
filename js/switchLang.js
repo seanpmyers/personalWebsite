39200b32c2bfa201975d2ffa05cfb9e3aef11534
Sean <spmyers@protonmail.com>
2018-03-20 19:45:28 -0400

    update to the home page text. update to file hierarchy. improvements to code structure/readability. update to page layout of all pages

--- js/switchLang.js
+++ js/switchLang.js
@@ -2,21 +2,23 @@
 zh = false;
 function switchLang (){
     if(!zh) {
-        document.getElementById('text').innerHTML = "孟尚朴";
-        document.getElementById('text2').innerHTML = "Eng/英文";
-        document.getElementById('text3').innerHTML = "博客";
-        document.getElementById('text4').innerHTML = "计划";
-        document.getElementById('text5').innerHTML = "资讯";
-        document.getElementById('text6').innerHTML = "主页";
+        //Nav bar translations
+        document.getElementById('name').innerHTML = "孟尚朴";
+        document.getElementById('language').innerHTML = "Eng/英文";
+        document.getElementById('blog').innerHTML = "博客";
+        document.getElementById('projects').innerHTML = "计划";
+        document.getElementById('about').innerHTML = "资讯";
+        document.getElementById('home').innerHTML = "主页";
         zh = true;
     }
     else {
-        document.getElementById('text').innerHTML = "Sean Myers";
-        document.getElementById('text2').innerHTML = "中文/Zh";
-        document.getElementById('text3').innerHTML = "Blog";
-        document.getElementById('text4').innerHTML = "Projects";
-        document.getElementById('text5').innerHTML = "About";
-        document.getElementById('text6').innerHTML = "Home";
+        //Nav bar translations
+        document.getElementById('name').innerHTML = "Sean Myers";
+        document.getElementById('language').innerHTML = "中文/Zh";
+        document.getElementById('blog').innerHTML = "Blog";
+        document.getElementById('projects').innerHTML = "Projects";
+        document.getElementById('about').innerHTML = "About";
+        document.getElementById('home').innerHTML = "Home";
         zh = false;
     }
 }
