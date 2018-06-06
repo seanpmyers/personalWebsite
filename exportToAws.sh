7784309cf20636419466d95839dca1fd8962bcfc
Sean <spmyers@protonmail.com>
2018-06-05 21:47:39 -0400

    created blog post template and started write up for auxcord. still need to finish default blog page.

--- exportToAws.sh
+++ exportToAws.sh
@@ -3,3 +3,5 @@ cp index.html awsExport/index
 cp html/about.html awsExport/about
 cp html/blog.html awsExport/blog
 cp html/projects.html awsExport/projects
+cp html/blog-posts/blog-auxCord.html awsExport/blog-auxCord
+cp html/blog-posts/blog-personalWebsite.html awsExport/blog-personalWebsite
